4f9cffe0f1b494fdfa1d6a2972ac6961acb6b664
leonov-serg-v <leonov.serg.v@gmail.com>
2021-10-24 18:20:57 +0300

    2. Репозиторий под проект Tracker [#245623]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,7 +5,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
-
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
