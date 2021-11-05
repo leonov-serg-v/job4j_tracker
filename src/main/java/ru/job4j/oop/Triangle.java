7a42fafe2e3972f2e86e37625ea3137c98b279a3
leonov-serg-v <55883885+leonov-serg-v@users.noreply.github.com>
2021-11-05 11:19:12 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #224205] (перепоставка)

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -16,10 +16,7 @@ public class Triangle {
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        if (ab < ac + bc && ac < ab + bc && bc < ac + ab) {
-            return true;
-        }
-        return false;
+        return (ab < ac + bc && ac < ab + bc && bc < ac + ab);
     }
 
     public double area() {
