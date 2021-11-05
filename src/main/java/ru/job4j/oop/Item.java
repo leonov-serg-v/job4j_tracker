392a86842b0ed5c8f57bb0856951c8285caf4bd9
leonov-serg-v <55883885+leonov-serg-v@users.noreply.github.com>
2021-11-05 12:58:04 +0300

    2. Перегрузка конструктора [#173890]

---
+++ src/main/java/ru/job4j/oop/Item.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
