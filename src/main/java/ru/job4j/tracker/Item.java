d199c8e94d79c81ff38250d80e0c922e1bf11223
leonov-serg-v <55883885+leonov-serg-v@users.noreply.github.com>
2021-11-07 16:41:30 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
 
     public int getId() {
         return id;
@@ -19,4 +23,9 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
 }
