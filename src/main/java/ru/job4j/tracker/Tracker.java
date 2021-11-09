9f24d36c877aaae966ff7f6518c9b3d74834080a
leonov-serg-v <55883885+leonov-serg-v@users.noreply.github.com>
2021-11-09 20:47:56 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,15 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
@@ -14,15 +23,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int i = indexOf(id);
+        return i != -1 ? items[i] : null;
     }
 
     public Item[] findAll() {
@@ -40,5 +42,14 @@ public class Tracker {
              }
         }
         return Arrays.copyOf(result, sizeResult);
+    }
+
+    public boolean replace(int id, Item anotherItem) {
+        int i = indexOf(id);
+        if (i != -1) {
+            anotherItem.setId(id);
+            items[i] = anotherItem;
+        }
+        return i != -1;
     }
 }
