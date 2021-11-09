99adcea4a8f9066111f09f1c62732752cb8c548e
leonov-serg-v <55883885+leonov-serg-v@users.noreply.github.com>
2021-11-09 20:13:52 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,22 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int sizeResult = 0;
+        Item item;
+        for (int i = 0; i < size; i++) {
+             item = items[i];
+             if (item.getName().equals(key)) {
+                 result[sizeResult++] = item;
+             }
+        }
+        return Arrays.copyOf(result, sizeResult);
     }
 }
