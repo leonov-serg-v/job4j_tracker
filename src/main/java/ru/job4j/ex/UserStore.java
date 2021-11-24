98116c8f884f073034843995f3f58e2e2a755f99
leonov-serg-v <leonov.serg.v@gmail.com>
2021-11-24 20:11:16 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #232092](перепоставка)

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -11,10 +11,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid()) {
-            throw new UserInvalidException("Пользователь не валидный");
-        }
-        if (user.getUsername().length() < 3) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("Пользователь не валидный");
         }
         return true;
