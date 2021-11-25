c9b4051a0071bf83e6eec8a573eab50779bac4d4
leonov-serg-v <leonov.serg.v@gmail.com>
2021-11-25 20:19:00 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #232471](перепоставка)

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,15 +2,14 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        if (n > 1) {
-            return calc(n - 1) * n;
-        } else {
+        if (n == 0 || n == 1) {
             return 1;
         }
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
-        int rsl = calc(1);
+        int rsl = calc(3);
         System.out.println(rsl);
     }
 }
