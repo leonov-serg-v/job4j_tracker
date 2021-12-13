1e6305175d25a8c7049487c8d4598a2f777e5705
leonov-serg-v <leonov.serg.v@gmail.com>
2021-12-13 21:04:53 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String word : text) {
+            if (!check.contains(word)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
