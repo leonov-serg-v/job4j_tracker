3c4eedb084506677a75dff49c8ed128699364a8c
leonov-serg-v <leonov.serg.v@gmail.com>
2021-12-15 18:55:09 +0300

    1. Паспорт и жители [#212656 #240068](перепоставка)

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -4,6 +4,7 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class PassportOfficeTest {
@@ -15,4 +16,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDubl() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(new Citizen("2f44a", "Leonov Serg")));
+    }
+
 }
