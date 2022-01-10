04d3a7b8e74a22cccdfb0cb3482714b6e9bdcd4d
leonov-serg-v <leonov.serg.v@gmail.com>
2022-01-10 21:58:52 +0300

    2. Отсортировать департаменты [#34352] перепоставка

--- src/main/java/ru/job4j/collection/TestArrayList.java
+++ src/main/java/ru/job4j/collection/TestArrayList.java
@@ -3,7 +3,7 @@ package ru.job4j.collection;
 import java.util.ArrayList;
 
 public class TestArrayList {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         String t1 = "test1";
         String t2 = "test2";
