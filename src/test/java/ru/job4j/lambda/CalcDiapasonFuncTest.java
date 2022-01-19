355770b5345aee22eee60f4f8e76c5c9b7f01fa9
leonov-serg-v <leonov.serg.v@gmail.com>
2022-01-19 21:25:50 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/test/java/ru/job4j/lambda/CalcDiapasonFuncTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.lambda;
+
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalcDiapasonFuncTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = CalcDiapasonFunc.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        List<Double> result = CalcDiapasonFunc.diapason(5, 8, x -> Math.pow(x, 2));
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenIndicativeFunctionThenIndicativeResults() {
+        List<Double> result = CalcDiapasonFunc.diapason(5, 8, x -> Math.pow(x, x));
+        List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
+        assertThat(result, is(expected));
+    }
+
+}
